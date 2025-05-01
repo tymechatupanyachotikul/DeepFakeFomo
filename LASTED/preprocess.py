16e11c9d67db9e1120ed9c0e974c70f21e931f57
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2025-05-01 13:27:31 +0200

    edit preprocess to allow choosing between reconstruct as fake images or original fake images

--- LASTED/preprocess.py
+++ LASTED/preprocess.py
@@ -6,13 +6,18 @@ from tqdm import tqdm
 data_root = 'GenImage/'
 save_path = 'annotation/'
 
-def FileList_Generation(dataset_name, split):
+def FileList_Generation(dataset_name, split, reconstructed, reconstruct_only):
     # Update your data path below
-    data_name_list = [
-        # e.g., (path_to_dataset, label)
-        (f'{dataset_name}/{split}/ai', 1),  # Fake Dataset
-        (f'{dataset_name}/{split}/nature', 0),  # Real Dataset
-    ]
+    if reconstruct_only:
+        data_name_list = [
+            (f'{dataset_name}/{split}/nature_reconstruct', 1)
+        ]
+    else:
+        data_name_list = [
+            # e.g., (path_to_dataset, label)
+            (f'{dataset_name}/{split}/nature_reconstruct', 1) if reconstructed else (f'{dataset_name}/{split}/ai', 1),  # Fake Dataset
+            (f'{dataset_name}/{split}/nature', 0),  # Real Dataset
+        ]
 
     img_list = []
     for data_name, label in data_name_list:
@@ -37,7 +42,10 @@ def FileList_Generation(dataset_name, split):
     img_list = img_list + img_list2
 
     print('#Images: %d' % len(img_list))
-    textfile = open(f'{save_path}{split}_{dataset_name}_num{len(img_list)}.txt', 'w')
+    if reconstructed:
+        textfile = open(f'{save_path}reconstruct_{dataset_name}.txt', 'w')
+    else:
+        textfile = open(f'{save_path}{split}_{dataset_name}{"_reconstructed" if reconstructed else ""}.txt', 'w')
     for item in img_list:
         textfile.write('%s %s\n' % (item[0], item[1]))
     textfile.close()
@@ -52,7 +60,11 @@ if __name__ == '__main__':
                         help='name of dataset')
     parser.add_argument('--split', default='', type=str,
                         help='dataset split')
+    parser.add_argument('--reconstructed', default=True, type=str,
+                        help='use reconstructed dataset as fake images')
+    parser.add_argument('--reconstruct_only', default=False, type=str,
+                        help='extract only reconstructed images')
 
     args = parser.parse_args()
 
-    FileList_Generation(args.dataset_name, args.split)
+    FileList_Generation(args.dataset_name, args.split, args.reconstructed)
